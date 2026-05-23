db5debb38adadc34f536eefba3133cf9bdab8dca
sourabh-969 <sourabhsaroj1@gmail.com>
2026-05-23 14:02:15 +0530

    P_boolean & if...else statement
    practice Boolean & if else.

---
+++ javascript/boolean.js
@@ -0,0 +1,7 @@
+let isvegan = false
+if (isvegan){
+    console.log("yes, i'm vegan")
+}
+else{
+    console.log("no, i'm not vegan")
+}
--- p-5_blackJack/index.js
+++ p-5_blackJack/index.js
@@ -1,15 +1,49 @@
-let firstcard = 6
-let secondcard = 9
+let firstcard = 6;
+let secondcard = 9;
 
 let sum = firstcard + secondcard;
 
-if(sum<=20){
-    console.log("Do you want to draw a new card")
-}
-else if (sum === 21){
-        console.log("Wohoo! You've got Blackjack")
-}
-else{
-    console.log("you are out of the Game!")
+let hasBlackJack = false;
+let isAlive = true;
 
+let message = "";
+
+let messageEl = document.getElementById("message-el");
+console.log(messageEl);
+
+let cardsEl = document.getElementById("card-el");
+console.log(cardsEl);
+
+let sumEl = document.querySelector("#sum-el");
+
+function startGame() {
+  renderGame();
+}
+
+function renderGame() {
+  cardsEl.textContent += firstcard + " & " + secondcard;
+  sumEl.textContent += sum;
+
+  if (sum <= 20) {
+    message = "Do you want to draw a new card";
+  } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack";
+    hasBlackJack = true;
+  } else {
+    message = "you are out of the Game!";
+    isAlive = false;
+  }
+
+  messageEl.textContent = message;
+  console.log(message);
+  console.log("hasBlackJack: " + hasBlackJack);
+  console.log("isAlive: " + isAlive);
+}
+
+function newCard() {
+  let variableCard = 6;
+
+  sum = sum + variableCard;
+
+  renderGame();
 }
